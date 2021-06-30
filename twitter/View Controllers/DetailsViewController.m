f221928df1f7ee2145f1d976b5b20e325d76d3fe
constance19 <67133685+constance19@users.noreply.github.com>
2021-06-29 19:24:15 -0700

    autolayout for detail view

--- twitter/View Controllers/DetailsViewController.m
+++ twitter/View Controllers/DetailsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "DetailsViewController.h"
+#import "APIManager.h"
 
 @interface DetailsViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *profileImage;
@@ -28,8 +29,16 @@
     [super viewDidLoad];
     
     // Do any additional setup after loading the view.
+    
+    // Set User object attributes for labels
     self.authorLabel.text = self.tweet.user.name;
-    self.usernameLabel.text = self.tweet.user.screenName;
+    NSString* username = self.tweet.user.screenName;
+    // Append '@' to the beginning of the retrieved username
+    if (username) {
+        self.usernameLabel.text = [@"@" stringByAppendingString:username];
+    }
+    
+    // Set Tweet object attributes for labels
     self.tweetLabel.text = self.tweet.text;
     self.timeLabel.text = self.tweet.time;
     self.dateLabel.text = self.tweet.date;
@@ -37,9 +46,119 @@
     // Set Tweet object attributes for buttons
     NSString* retweet = [NSString stringWithFormat:@"%i", self.tweet.retweetCount];
     NSString* favorite = [NSString stringWithFormat:@"%i", self.tweet.favoriteCount];
+    self.retweetCount.text = retweet;
+    self.favoriteCount.text = favorite;
     
-    [self.retweetButton setTitle:retweet forState:UIControlStateNormal];
-    [self.favoriteButton setTitle:favorite forState:UIControlStateNormal];
+    // Set the user's profile image
+    NSString *URLString = self.tweet.user.profilePicture;
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    self.profileImage.image = [[UIImage alloc] initWithData:urlData];
+    
+    // Set display of buttons
+    if (self.tweet.favorited) {
+        [self.favoriteButton setSelected:YES];
+    } else {
+        [self.favoriteButton setSelected:NO];
+    }
+    
+    if (self.tweet.retweeted) {
+        [self.retweetButton setSelected:YES];
+    } else {
+        [self.retweetButton setSelected:NO];
+    }
+}
+
+- (IBAction)didTapRetweet:(id)sender {
+    // Unretweet
+    if (self.tweet.retweeted) {
+        // Update the local tweet model
+        self.tweet.retweeted = NO;
+        self.tweet.retweetCount -= 1;
+        
+        // Update cell UI
+        NSString *retweetCount = [NSString stringWithFormat:@"%i", self.tweet.retweetCount];
+        self.retweetCount.text = retweetCount;
+        [self.retweetButton setSelected:NO];
+        
+        
+        // Send a POST request to the POST retweets/create endpoint
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error){
+                  NSLog(@"Error unretweeting tweet: %@", error.localizedDescription);
+             }
+             else{
+                 NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+             }
+         }];
+    
+    // Retweet
+    } else {
+        // Update the local tweet model
+        self.tweet.retweeted = YES;
+        self.tweet.retweetCount += 1;
+        
+        // Update cell UI
+        NSString *retweetCount = [NSString stringWithFormat:@"%i", self.tweet.retweetCount];
+        self.retweetCount.text = retweetCount;
+        [self.retweetButton setSelected:YES];
+        
+        
+        // Send a POST request to the POST retweets/create endpoint
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error){
+                  NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+             }
+             else{
+                 NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+             }
+         }];
+    }
+}
+
+- (IBAction)didTapFavorite:(id)sender {
+    // Unfavorite
+    if (self.tweet.favorited) {
+        // Update the local tweet model
+        self.tweet.favorited = NO;
+        self.tweet.favoriteCount -= 1;
+        
+        // Update view UI
+        NSString *favCount = [NSString stringWithFormat:@"%i", self.tweet.favoriteCount];
+        self.favoriteCount.text = favCount;
+        [self.favoriteButton setSelected:NO];
+        
+        // Send a POST request to the POST favorites/create endpoint
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error){
+                  NSLog(@"Error unfavoriting tweet: %@", error.localizedDescription);
+             }
+             else{
+                 NSLog(@"Successfully unfavorited the following Tweet: %@", tweet.text);
+             }
+         }];
+    
+    // Favorite
+    } else {
+        // Update the local tweet model
+        self.tweet.favorited = YES;
+        self.tweet.favoriteCount += 1;
+        
+        // Update cell UI
+        NSString *favCount = [NSString stringWithFormat:@"%i", self.tweet.favoriteCount];
+        self.favoriteCount.text = favCount;
+        [self.favoriteButton setSelected:YES];
+        
+        // Send a POST request to the POST favorites/create endpoint
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+             if(error){
+                  NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+             }
+             else{
+                 NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+             }
+         }];
+    }
 }
 
 /*
