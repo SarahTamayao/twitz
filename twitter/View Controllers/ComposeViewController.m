93169bc8f1b3705868943d8868c77df723b4f96c
constance19 <67133685+constance19@users.noreply.github.com>
2021-06-29 19:35:36 -0700

    added character count for compose tweet
    required: all complete
    optional: unretweet+unfavorite, character count complete

--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -11,6 +11,7 @@
 
 @interface ComposeViewController () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextView *composeTweet;
+@property (weak, nonatomic) IBOutlet UILabel *charCount;
 
 @end
 
@@ -31,6 +32,9 @@
     // Set placeholder text for the compose tweet box
     self.composeTweet.text = @"What's happening?";
     self.composeTweet.textColor = [UIColor lightGrayColor];
+    
+    // Set initial character count of 0
+    self.charCount.text = @"0";
 }
 
 // Cancel button in navigation bar
@@ -80,6 +84,22 @@
         textView.textColor = [UIColor lightGrayColor]; //optional
     }
     [textView resignFirstResponder];
+}
+
+// For character count feature
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    // Set the max character limit
+    int characterLimit = 140;
+    
+    // Construct what the new text would be if we allowed the user's latest edit
+    NSString *newText = [self.composeTweet.text stringByReplacingCharactersInRange:range withString:text];
+    
+    // Update character count label
+    NSString* characters = [NSString stringWithFormat:@"%i", newText.length];
+    self.charCount.text = characters;
+    
+    // Allow or disallow the new text
+    return newText.length < characterLimit;
 }
 
 /*
